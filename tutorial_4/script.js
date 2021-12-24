3ed25366001b742dc43515ae0e2c3b9df8db331b
Muhamed Dadozhonov <92201434+SektorD@users.noreply.github.com>
2021-12-24 16:05:55 +0500

    Add files via upload

--- tutorial_4/script.js
+++ tutorial_4/script.js
@@ -1,7 +1,22 @@
-var a = '1';
-var b = '2';
-var c = a + b;
-var d = false;
+var catNames = ['Pushok', 'Marsik', 'Marsik', 'Tusik'];
+var catColor = ['Black', 'Orange', 'White']
+var catLen = [30, 25, 20]
 
-if (a === 1) {console.log('!!!');}
-else {console.log('...');}
+var arrayCats = [];
+
+for (var i = 0; i < random(1, 12); i++) {
+	var cat = {
+	name: catNames[random(0, catNames.length - 1)],
+	color: catColor[random(0, catColor.length - 1)],
+	len: catLen[random(0, catLen.length - 1)],
+};
+	arrayCats.push(cat);
+}
+
+for (var i = 0; i < arrayCats.length; i++) {
+	console.log(arrayCats[i]);
+}
+
+function random(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
