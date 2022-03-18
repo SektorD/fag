1a51897699616aefcf6980fa208caa2523f86bcb
SektorD <dadozonov03@gmail.com>
2022-03-18 22:55:38 +0500

    ...

--- tasks/t1.js
+++ tasks/t1.js
@@ -1,20 +1,16 @@
-<<<<<<< HEAD
-function getResult(s) {
- return s[0].toUpperCase() + s.substring(1, s.c);
-}
+// function getResult(string) {
+//  return string[0].toUpperCase() + string.substring(1, string.length);
+// }
 
-console.log(
-    getResult('имя'),'==','Имя'
-);
+// console.log(getResult('имя'),'==','Имя');
 
-// function getResult(a) {
+
+
+// function getResult(array) {
 //     var s = 0;
-
 //     for(let i = 0; i < a.length; i++) {
-//         s =s + a[i];
-
+//         s =s + array[i];
 //     }
-
 //     return s;
 // }
 
@@ -22,63 +18,40 @@ console.log(
 // console.log(getResult([-1, -2, -3, -4, -5]), '==', -15)
 // console.log(getResult([-2, -2, 0, 4]), '==', 0)
 
-function getResult(a) {
-    var a = ['0','1','2','3','4','5',
-                '6','7','8','9'];
-    for (let i = 0; i < a.length; i++) {
-        s = s.replace(a[i], '!');
-    }
 
-    return s;
 
-}
+// function getResult(a) {
+//     var a = ['0','1','2','3','4','5',
+//                 '6','7','8','9'];
+//     for (let i = 0; i < a.length; i++) {
+//         s = s.replace(a[i], '!');
+//     }
 
-console.log(
-    getResult('Текст с числами 2 и 3 до 5.'),
-    '==',
-    'Текст с числами ! и ! до !.'
-);
-=======
+//     return s;
 
-// function getResult(s) {
-//  return s[0].toUpperCase() + s.substring(1, s.lenght);
 // }
 
 // console.log(
-//     getResult('имя'),
+//     getResult('Текст с числами 2 и 3 до 5.'),
 //     '==',
-//     'Имя'
+//     'Текст с числами ! и ! до !.'
 // );
 
-// function getResult(a) {
-//     var s = 0;
 
-//     for(let i = 0; i < a.lenght; i++) {
-//         s =s + a[i];
 
-//     }
-
-//     return s;
-// }
-
-// console.log(getResult([1, 2, 3, 4, 5]), '==', 15)
-// console.log(getResult([-1, -2, -3, -4, -5]), '==', -15)
-// console.log(getResult([-2, -2, 0, 4]), '==', 0)
-
-function getResult(a) {
-    var a = ['0','1','2','3','4','5',
-                '6','7','8','9'];
-    for (let i = 0; i < a.lenght; i++) {
-        s = s.replace(a[i], '!');
+function getResult(string){
+    var symbols = {
+        "А": "0","Б": "1","В": "2","Г": "3","Д": "4","Е": "5",
+        "Ё": "6","Ж": "7","З": "8","И": "9","Й": "10","К": "11",
+        "Л": "12","М": "13","Н": "14","О": "15","П": "16","Р": "17",
+        "С": "18","Т": "19","У": "20","Ф": "21","Х": "22","Ц": "23",
+        "Ч": "24","Ш": "25","Щ": "26","Ъ": "27","Ы": "28","Ь": "29",
+        "Э": "30","Ю": "31","Я": "32"
     }
+    var str = string;
+    var numbers = str.replace(/./gi,$0=>symbols[$0.toUpperCase()]||$0);
 
-    return s;
-
+    return numbers;
 }
 
-console.log(
-    getResult('Текст с числами 2 и 3 до 5.'),
-    '==',
-    'Текст с числами ! и ! до !.'
-);
->>>>>>> 74bda0a48889042b7bfb9fe26b5a0614c6cf1afb
+console.log(getResult('А.р.б.у.з'), '==', '0.17.1.20.8');
